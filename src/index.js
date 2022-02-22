02d5f1eb1ff2d9cf03d25f0852ca0cf6ecef7c55
Roman Baukin <rbaukin1984@gmail.com>
2022-02-22 17:23:04 +0400

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openBrackets = [];
+  const bracketsPair = {};
+
+  bracketsConfig.forEach((item) => {
+    openBrackets.push(item[0]);
+    bracketsPair[item[1]] = item[0];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    const currentSymbol = str[i];
+
+    if (openBrackets.includes(currentSymbol) && Object.keys(bracketsPair).includes(currentSymbol)) {
+      if (stack[stack.length - 1] === currentSymbol) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (bracketsPair[currentSymbol] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
